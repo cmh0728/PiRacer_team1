b1107c2b141c03590e62bbe87c8ef3fc6829dd2d
jjangddung <jdm0616@naver.com>
2025-07-21 12:21:44 +0200

    CAN-commu

--- src/Arduino/can_speed/can_speed.ino
+++ src/Arduino/can_speed/can_speed.ino
@@ -1,32 +1,27 @@
 #include <SPI.h>
-#include <mcp_can.h>
+#include "mcp2515_can.h"  // MCP2515 library
 
-// -----------------------------
-// 전역 변수 및 상수
-// -----------------------------
-volatile int pulseCount = 0;           // 펄스 카운터
-unsigned long lastTime = 0;            // 마지막 측정 시간
+
+volatile int pulseCount = 0;           // pulse
+unsigned long lastTime = 0;            
 int rpm = 0;
 
-const int SPI_CS_PIN = 9;               
-MCP_CAN CAN(SPI_CS_PIN);                
-
+const int SPI_CS_PIN = 9;
+mcp2515_can CAN(SPI_CS_PIN);  
 
 void countPulse() {
   pulseCount++;
 }
 
-
-
-
 void setup() {
   Serial.begin(9600);
-  
-  while (CAN_OK != CAN.begin(MCP_STDEXT, CAN_500KBPS, MCP_8MHZ)) {
-    Serial.println("CAN retry..");
+
+  // MCP2515  () 500kbps + 16MHz)
+  while (CAN.begin(CAN_500KBPS) != CAN_OK) {
+    Serial.println("CAN retry...");
     delay(100);
   }
-  Serial.println("CAN success");
+  Serial.println("CAN init success");
 
   pinMode(3, INPUT_PULLUP); // D3 pin
   attachInterrupt(digitalPinToInterrupt(3), countPulse, FALLING);
@@ -35,26 +30,24 @@ void setup() {
 void loop() {
   unsigned long currentTime = millis();
 
-  if (currentTime - lastTime >= 1000) { 
+  if (currentTime - lastTime >= 1000) {
     noInterrupts();
     int count = pulseCount;
     pulseCount = 0;
     interrupts();
 
-    rpm = count * 60; 
+    rpm = count * 60;
 
     Serial.print("RPM: ");
     Serial.println(rpm);
 
-    
     byte rpmBytes[2];
-    rpmBytes[0] = (rpm >> 8) & 0xFF;  
-    rpmBytes[1] = rpm & 0xFF;         
+    rpmBytes[0] = (rpm >> 8) & 0xFF;
+    rpmBytes[1] = rpm & 0xFF;
 
-    
     byte sendResult = CAN.sendMsgBuf(0x100, 0, 2, rpmBytes);
     if (sendResult == CAN_OK) {
-      Serial.println("CAN  message success");
+      Serial.println("CAN message success");
     } else {
       Serial.println("CAN message fail");
     }
