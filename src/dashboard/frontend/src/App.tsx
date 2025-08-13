e444d63567bafdca286d13c3bddfeaa715755a67
cmh0728 <gambaro344@gmail.com>
2025-08-13 21:47:03 +0200

    edit server status

--- src/dashboard/frontend/src/App.tsx
+++ src/dashboard/frontend/src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
     // 화면 중앙 배치
     <div className="min-h-dvh grid place-items-center bg-gray text-slate-900">
       {/* 16:9 비율 보드: 뷰포트에 맞춰 자동 스케일 */}
-      <div className="relative w-[min(100vw,177.78vh)] aspect-[16/13] rounded-3xl border bg-neutral-900 shadow">
+      <div className="relative w-[min(100vw,177.78vh)] aspect-[21/12] rounded-3xl border bg-neutral-900 shadow">
         {/* 내부는 퍼센트 단위로 → 비율 유지 */}
         {/* 내부 레이아웃: 배지 / 카메라 / 배지 / 하단 카드 */}
         <div className="absolute inset-0 p-[2%] grid grid-rows-[auto_60%_auto_1fr] gap-[2%]">
@@ -35,10 +35,19 @@ export default function App() {
           </div>
 
           {/* 하단 카드 */}
-          <div className="grid grid-cols-3 gap-[2%]">
+          <div className="grid grid-cols-4 gap-[2%]">
             <Card title="RPM" value={Math.round(t.rpm)} />
             <Card title="Speed" value={typeof t.speed === "number" ? t.speed.toFixed(1) : "0.0"}  unit="[cm/s]" />
-            <Card title="Battery" value={t.battery} unit="[%]" />
+            {/* 배터리 바 */}
+            <div className="p-4 rounded-2xl border bg-neutral-800 shadow-sm">
+              <div className="text-sm text-neutral-400 mb-2 text-[max(20px,1.4vmin)]">Battery</div>
+              <div className="w-full bg-neutral-700 rounded-full h-3 overflow-hidden">
+                <div className="bg-emerald-500 h-3 transition-all duration-300 "
+                  style={{ width: `${t.battery}%` }}
+                ></div>
+              </div>
+              <div className="text-sm text-neutral-300 mt-1 text-[max(15px,1.4vmin)]">{t.battery} %</div>
+            </div>
             <Card title="Gear" value={t.gear}/>
           </div>
         </div>
