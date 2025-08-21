b7b9c5c8264d5db8f83a3010833202a3b34c4ed1
cmh0728 <gambaro344@gmail.com>
2025-08-21 12:53:06 +0200

    edit dis file for dashboard

--- src/dashboard/frontend/src/lib/telemetry.ts
+++ src/dashboard/frontend/src/lib/telemetry.ts
@@ -3,4 +3,6 @@ export type Telemetry = {
   speed: number;   // cm/s
   battery: number; // 0~100 %
   gear : string; // 3 : p, 1 : D , 0 : N , 2 : R 
+  cpu?: number;   // CPU usage (%)
+  net?: boolean;  // 네트워크 연결 여부
 };
